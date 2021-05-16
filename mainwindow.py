f631a9ab3007bf07f6cc66ca4954138da7c6a10a
Iván L. Murillo <diskman199@gmail.com>
2021-05-16 11:28:57 -0500

    Read image as np array and ccreate new image

--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,10 @@
+from PySide2 import QtCore
 from PySide2.QtWidgets import QMainWindow, QFileDialog ,QMessageBox
 from PySide2.QtCore import Slot
+from PySide2.QtGui import QPixmap
 from ui_mainwindow import Ui_MainWindow
+import numpy as np
+from PIL import Image
 
 class MainWindow(QMainWindow):
 
@@ -10,9 +14,37 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.load_image.clicked.connect(self.load_image)
+        self.ui.encrypt_open.clicked.connect(self.load_image)
+        
+    def encrypt_image(self,location):
+        image = Image.open(location)
+        print('Format: ' + image.format)
+        print('Size: ' + str(image.size))
+        print('Mode: ' + image.mode)
 
-    
+        data = np.asarray(image)
+        print(len(data))
+        print(data.shape)
+
+        key = '123'
+
+        rows = []
+        for dimension in data:          #Loop through x axix
+            columns = []
+            for pixel in dimension:     #Loop through y axis
+                colors = []
+                for color in pixel:     #Loop through channels
+                    colors.append(0)
+                columns.append(colors)
+            rows.append(columns)
+        
+        print(len(rows))
+        print(len(rows[0]))
+        print(len(rows[0][0]))
+
+    def code_8_bit(self,key):
+        print(key)
+
     @Slot()
     def load_image(self):
         location = QFileDialog.getOpenFileName(
@@ -28,6 +60,13 @@ class MainWindow(QMainWindow):
                 "Éxito",
                 "Se abrió el archivo " + location
             )
+            w = self.ui.encrypt_original.width()
+            h = self.ui.encrypt_original.height()
+
+            image  = QPixmap(location)
+            self.ui.encrypt_original.setPixmap(image.scaled(w,h,QtCore.Qt.KeepAspectRatio))
+            self.ui.encrypt_original.setMask(image.mask())
+            self.encrypt_image(location)
 
         else:
             QMessageBox.critical(
--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -17,21 +17,52 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(630, 390)
+        MainWindow.resize(716, 466)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.original_image = QLabel(self.centralwidget)
-        self.original_image.setObjectName(u"original_image")
-        self.original_image.setGeometry(QRect(190, 30, 240, 240))
-        self.original_image.setFrameShape(QFrame.WinPanel)
-        self.original_image.setFrameShadow(QFrame.Sunken)
-        self.load_image = QPushButton(self.centralwidget)
-        self.load_image.setObjectName(u"load_image")
-        self.load_image.setGeometry(QRect(280, 300, 101, 31))
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.gridLayout = QGridLayout(self.tab)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.encrypt_original = QLabel(self.tab)
+        self.encrypt_original.setObjectName(u"encrypt_original")
+        self.encrypt_original.setFrameShape(QFrame.WinPanel)
+        self.encrypt_original.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout.addWidget(self.encrypt_original, 0, 0, 1, 1)
+
+        self.encrypt_result = QLabel(self.tab)
+        self.encrypt_result.setObjectName(u"encrypt_result")
+        self.encrypt_result.setFrameShape(QFrame.WinPanel)
+        self.encrypt_result.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout.addWidget(self.encrypt_result, 0, 1, 1, 1)
+
+        self.encrypt_open = QPushButton(self.tab)
+        self.encrypt_open.setObjectName(u"encrypt_open")
+
+        self.gridLayout.addWidget(self.encrypt_open, 1, 0, 1, 1)
+
+        self.encrypt_save = QPushButton(self.tab)
+        self.encrypt_save.setObjectName(u"encrypt_save")
+
+        self.gridLayout.addWidget(self.encrypt_save, 1, 1, 1, 1)
+
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.gridLayout_2.addWidget(self.tabWidget, 0, 0, 1, 1)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 630, 21))
+        self.menubar.setGeometry(QRect(0, 0, 716, 21))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -39,12 +70,19 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
+        self.tabWidget.setCurrentIndex(0)
+
+
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.original_image.setText(QCoreApplication.translate("MainWindow", u"Original image", None))
-        self.load_image.setText(QCoreApplication.translate("MainWindow", u"Cargar imagen", None))
+        self.encrypt_original.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.encrypt_result.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.encrypt_open.setText(QCoreApplication.translate("MainWindow", u"Abrir imagen", None))
+        self.encrypt_save.setText(QCoreApplication.translate("MainWindow", u"Guardar imagen", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Encriptar", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Desencriptar", None))
     # retranslateUi
 
